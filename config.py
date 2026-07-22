3a561e50d0534cd654e33e5e34a3ada81d4db2ce
idellqer <sasakaul52@gmail.com>
2026-07-22 15:10:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-import os
+BOT_TOKEN = "8979038466:AAEeIgJ88AWdoGDJEBxma624_blHbzw8G9k"
 
-BOT_TOKEN = os.getenv(
-    "BOT_TOKEN",
-    "8979038466:AAEeIgJ88AWdoGDJEBxma624_blHbzw8G9k"
-)
-
-ADMIN_ID = int(os.getenv(
-    "ADMIN_ID",
-    "5812308083"
-))
+ADMIN_ID = 5812308083
