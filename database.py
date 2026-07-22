a9a761de9f14a04ddbadbd8ed964ca97c666f291
idellqer <sasakaul52@gmail.com>
2026-07-22 14:51:07 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,51 @@
 import sqlite3
-db=sqlite3.connect("requests.db")
-cur=db.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS requests(id INTEGER PRIMARY KEY,user_id INTEGER,amount TEXT,photo TEXT)")
-db.commit()
 
-def add_request(user_id, amount, photo):
-    cur.execute("INSERT INTO requests(user_id,amount,photo) VALUES(?,?,?)",(user_id,amount,photo))
+
+def connect():
+    return sqlite3.connect("database.db")
+
+
+def add_user(user_id):
+    db = connect()
+    cursor = db.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER UNIQUE
+    )
+    """)
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO users(user_id) VALUES (?)",
+        (user_id,)
+    )
+
     db.commit()
+    db.close()
+
+
+def save_request(user_id, amount, photo):
+    db = connect()
+    cursor = db.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS requests(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount TEXT,
+        photo TEXT,
+        status TEXT
+    )
+    """)
+
+    cursor.execute(
+        """
+        INSERT INTO requests(user_id, amount, photo, status)
+        VALUES(?,?,?,?)
+        """,
+        (user_id, amount, photo, "new")
+    )
+
+    db.commit()
+    db.close()
